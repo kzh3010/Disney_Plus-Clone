f017078b4f99b6f678023f017ed9f44393c15617
kzh3010 <zcvpal2@naver.com>
2023-07-12 16:29:17 +0900

    Feat: MovieModal 생성

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,8 +1,11 @@
 import React, { useCallback, useEffect, useState } from "react";
 import axios from "../api/axios";
 import "./Row.css";
+import MovieModal from "./MovieModal";
 const Row = ({ title, id, fetchUrl }) => {
   const [movies, setMovies] = useState([]);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [movieSelected, setMovieSelected] = useState({});
 
   const fetchMovieData = useCallback(async () => {
     const request = await axios.get(fetchUrl);
@@ -13,7 +16,11 @@ const Row = ({ title, id, fetchUrl }) => {
     fetchMovieData();
   }, [fetchMovieData]);
 
-  const handleClick = () => {};
+  const handleClick = (movie) => {
+    setModalOpen(true);
+    setMovieSelected(movie);
+  };
+
   return (
     <div>
       <h2>{title}</h2>
@@ -28,28 +35,31 @@ const Row = ({ title, id, fetchUrl }) => {
             {"<"}
           </span>
         </div>
+        <div id={id} className="row__posters">
+          {movies.map((movie) => (
+            <img
+              key={movie.id}
+              className="row__poster"
+              src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
+              alt={movie.name}
+              onClick={() => handleClick(movie)}
+            />
+          ))}
+        </div>
+        <div className="slider__arrow-right">
+          <span
+            className="arrow"
+            onClick={() => {
+              document.getElementById(id).scrollLeft -= window.innerWidth - 80;
+            }}
+          >
+            {">"}
+          </span>
+        </div>
       </div>
-      <div id={id} className="row__posters">
-        {movies.map((movie) => (
-          <img
-            key={movie.id}
-            className="row__poster"
-            src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
-            alt={movie.name}
-            onClick={() => handleClick(movie)}
-          />
-        ))}
-      </div>
-      <div className="slider__arrow-right">
-        <span
-          className="arrow"
-          onClick={() => {
-            document.getElementById(id).scrollLeft -= window.innerWidth - 80;
-          }}
-        >
-          {">"}
-        </span>
-      </div>
+      {modalOpen && (
+        <MovieModal {...movieSelected} setModalOpen={setModalOpen} />
+      )}
     </div>
   );
 };
