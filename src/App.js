8766e6985d6a91d3cfcf29e868230ca12bfc4b2f
kzh3010 <zcvpal2@naver.com>
2023-07-13 13:03:07 +0900

    Feat: 검색 기능, swiper

--- src/App.js
+++ src/App.js
@@ -1,55 +1,34 @@
-import { styled } from "styled-components";
+import { Outlet, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Nav from "./components/Nav";
-import Banner from "./components/Banner";
-import Category from "./components/Category";
-import Row from "./components/Row";
-import requests from "./api/request";
+import LoginPage from "./pages/LoginPage";
+import DetailPage from "./pages/DetailPage";
+import MainPage from "./pages/MainPage";
+import SearchPage from "./pages/SearchPage";
+
+const Layout = () => {
+  return (
+    <div>
+      <Nav />
+
+      <Outlet />
+    </div>
+  );
+};
 
 function App() {
   return (
-    <Container>
-      <Nav />
-      <Banner />
-      <Category />
-      <Row title="Trending Now" id="TN" fetchUrl={requests.fetchTrending} />
-      <Row title="Top Rated" id="TR" fetchUrl={requests.fetchTopRated} />
-      <Row
-        title="Action Movies"
-        id="AM"
-        fetchUrl={requests.fetchActionMovies}
-      />
-      <Row
-        title="Comedy Movies"
-        id="CM"
-        fetchUrl={requests.fetchComedyMovies}
-      />
-    </Container>
+    <div className="app">
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<LoginPage />} />
+          <Route path="main" element={<MainPage />} />
+          <Route path=":movieId" element={<DetailPage />} />
+          <Route path="search" element={<SearchPage />} />
+        </Route>
+      </Routes>
+    </div>
   );
 }
 
 export default App;
-
-const Container = styled.main`
-  position: relative;
-
-  /* calc() CSS 함수를 사용하면 CSS 속성의 값으로 계산식을 지정 */
-  min-height: calc(100vh - 250px);
-  /* overflow 속성은 요소의 박스에 내용이 더 길때, 어떻게 보일지 선택하는 속성 */
-  /* overflow-x 속성은 x축, 즉 왼쪽과 오른쪽의 내용이 더 길 때(가로) */
-  /* 부모요소의 범위를 넘어가는 자식 요소의 부분은 보이지 않게 */
-  overflow-x: hidden;
-  display: block;
-  top: 72px;
-  padding: 0 calc(3.5vw + 5px);
-  /* 가상 요소 after를 이용하여 이미지를 백그라운데 넣기 */
-  &:after {
-    background: url("/images/home-background.png") center center / cover
-      no-repeat fixed;
-    content: "";
-    position: absolute;
-    inset: 0px;
-    opacity: 1;
-    z-index: -1;
-  }
-`;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,39 +1,86 @@
 import React, { useEffect, useState } from "react";
-import { styled } from "styled-components";
-
+import { useLocation, useNavigate } from "react-router-dom";
+import styled from "styled-components";
 const Nav = () => {
-  const [show, setShow] = useState(false);
-  useEffect(() => {
-    window.addEventListener("scroll", () => {
-      if (window.scrollY > 50) {
-        setShow(true);
-      } else {
-        setShow(false);
-      }
-    });
+  const [show, setShow] = useState("false");
+  const { pathname } = useLocation();
+  const [searchValue, setSearchValue] = useState("");
+  const navigate = useNavigate();
 
+  const handleChange = (e) => {
+    setSearchValue(e.target.value);
+    navigate(`/search?q=${e.target.value}`);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
     return () => {
-      window.removeEventListener("scroll", () => {});
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
+
+  const handleScroll = () => {
+    if (window.scrollY > 50) {
+      setShow("true");
+    } else {
+      setShow("false");
+    }
+  };
 
   return (
     <NavWrapper show={show}>
       <Logo>
         <img
+          alt="Disney Plus Logo"
           src="/images/logo.svg"
-          alt="Diseny Plus Logo"
           onClick={() => (window.location.href = "/")}
         />
       </Logo>
+      {pathname === "/" ? (
+        <Login>Login</Login>
+      ) : (
+        <Input
+          value={searchValue}
+          onChange={handleChange}
+          className="nav__input"
+          type="text"
+          placeholder="검색해 주세요"
+        />
+      )}
     </NavWrapper>
   );
 };
 
 export default Nav;
 
-const NavWrapper = styled.div`
+const Login = styled.a`
+  background-color: rgba(0, 0, 0, 0.6);
+  padding: 8px 16px;
+  text-transform: uppercase;
+  letter-spacing: 1.5px;
+  border: 1px solid #f9f9f9;
+  transition: all 0.2s ease 0s;
+
+  &:hover {
+    background-color: #f9f9f9;
+    border-color: transparent;
+    color: gray;
+  }
+`;
+
+const Input = styled.input`
   position: fixed;
+  left: 50%;
+  transform: translate(-50%, 0);
+  background-color: rgba(0, 0, 0, 0.582);
+  border-radius: 5px;
+  color: white;
+  padding: 5px;
+  border: none;
+`;
+
+const NavWrapper = styled.nav`
+  position: fixed; // 움직여도 위치 고정
   top: 0;
   left: 0;
   right: 0;
@@ -54,4 +101,9 @@ const Logo = styled.a`
   max-height: 70px;
   font-size: 0;
   display: inline-block;
+
+  img {
+    display: block;
+    width: 100%;
+  }
 `;
